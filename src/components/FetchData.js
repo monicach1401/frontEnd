a51f2f9601957948921a70aeb3755684292b574a
Monica Chavarria <monicachjob@gmail.com>
2023-08-27 11:17:11 +0200

    AddFilter

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -1,30 +1,66 @@
 import React, { useEffect, useState } from "react";
 import { NewCityForm } from '../components/NewCityForm';
+// importaciones para crear el Botón de Atrás
+import Button from '@mui/material/Button';// Importa el componente Button de Material-UI
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';// Importa el icono de Material-UI
+// importaciones para que funcione el botón
+import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 
 export const FetchData = () => {
-  const [cities, setCities] = useState([]);
-  const [city, setCity] = useState("");
+
+  //Función para volver atrás
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate("/");
+  }
+
+
+  // variables de Estado
+  const [cities, setCities] = useState([]); // estado para mostrar las ciudades de la base de datos
+  const [citySelected, setCitySelected] = useState(""); // estado que nos permmite guardar el valor introducido por el usuario en el input
   const [showNewCityForm, setShowNewCityForm] = useState(false); // Agregamos un estado para controlar la visibilidad del formulario
 
+
+  // Función que permite guardar en la variable de estado City el valor introducido por el usuario
   const selectCity = (citySelected) => {
-    setCity(citySelected);
+    setCitySelected(citySelected);
   }
 
   useEffect(() => {
     fetch("http://localhost:5000/cities/all")
       .then((response) => response.json())
       .then((data) => {
-        setCities(data);
+        setCities(data); // en la variable cities ya tenemos todos los datos de la base de datos
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
   }, []);
 
+
+  // Función que muestra en una lista de cards de las ciudades de la base de datos. Si el usuario no introduce nada las muestra todas y sino filtra por el 
+  // valor introducido 
   const ShowCities = () => {
-    return cities.map((city) => (
-      <li className="listCities" key={city._id}>
-        <h6> {city.name}, {city.country} <img src={city.img} alt="..." style={{ maxWidth: '100%', maxHeight: '300px' }} /></h6>
+
+    /* filteredCities una nueva lista que contiene solo las ciudades que coinciden con el valor del filtro. 
+    La función includes se utiliza para verificar si el nombre o el país de una ciudad incluyen el valor del filtro,
+    y se compara en minúsculas para hacer la comparación insensible a mayúsculas y minúsculas. */
+    const filteredCities = cities.filter(city => {
+      return city.name.toLowerCase().includes(citySelected.toLowerCase()) ||
+        city.country.toLowerCase().includes(citySelected.toLowerCase());
+    });
+    return filteredCities.map((citySelected) => (
+      <li className="listCities" key={citySelected._id}>
+        <div className="card ">
+          <div className="card-body">
+            <h6 className="card-text"> {citySelected.name}, {citySelected.country}  </h6>
+          </div>
+          <img src={citySelected.img} alt="..." style={{ maxWidth: '100%', maxHeight: '300px' }} />
+        </div>
       </li>
     ));
   };
@@ -33,18 +69,30 @@ export const FetchData = () => {
     <>
       <div className="filter-container">
         <h1 className="filter-title">Choose a City</h1>
+        {/* ACtualizamos la variable selectCity con el valor introducido por el usuario en el input*/}
         <input
           type="text"
           className="filter-input"
+          value={citySelected}
           onChange={e => selectCity(e.target.value)}
         />
-        <button className="myButton"onClick={() => setShowNewCityForm(true)}>Add New City</button> {/* Modificamos el onClick para cambiar el estado */}
+        <button className="myButton" onClick={() => setShowNewCityForm(true)}>Add New City</button> {/* Modificamos el onClick para cambiar el estado */}
       </div>
-     {/* Renderizamos el formulario si showNewCityForm es true y cuando los datos se envien se llamará a la función onSucess que ocultará el formulario*/}
-      {showNewCityForm && <NewCityForm hideForm={() => setShowNewCityForm(false)}/>} 
-      
+      {/* Renderizamos el formulario si showNewCityForm es true y cuando los datos se envien se llamará a la función onSucess que ocultará el formulario*/}
+      {showNewCityForm && <NewCityForm hideForm={() => setShowNewCityForm(false)} />}
+
       <div>
         <ul>{ShowCities()}</ul>
+      </div>
+
+      <div style={{ marginLeft: '20px', marginTop: '20px' }}>
+        <Button
+          variant="contained"
+          color="success"
+          startIcon={<ArrowBackIosNewIcon />}
+          onClick={goBack}
+        >
+        </Button>
       </div>
     </>
   );
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,24 +8,16 @@ export const Landing = () => {
 
     return (
         <>
-            <div className="d-flex flex-column bd-highlight mb-3">
-                <div className="Myitinary"><img className="Myitinary" src={MYtineraryLogo} alt="MYtinerary Logo" /></div>
-                <div className="description">
-                    <h5><strong>Find your perfect trip</strong>, designed by insiders who know and love their cities</h5>
-                    <div className="start-browsing">
-                        <h1>START BROWSING</h1>
-                    </div>
-                    <Link to="/cities">
-                        <img src={circledright2} alt="Circled Right" />
-                    </Link>
-                </div>
-                <div className="options">
-                    <h5>Want to build your own MYtinerary?</h5>
-                    <h5><a href="#">Login</a></h5>
-                    <h5><a href="#">Create Account</a></h5>
-                </div>
-            </div>
-            <div className="home-icon"><img src={homeIcon} alt="Home Icon" /></div>
+            <img className="Myitinary" src={MYtineraryLogo} alt="MYtinerary Logo" />
+            <p><strong>Find your perfect trip</strong>, designed by insiders who know and love their cities</p>
+            <h1>START BROWSING</h1>
+            <Link to="/cities">
+                <img src={circledright2} alt="Circled Right" />
+            </Link>
+            <h5>Want to build your own MYtinerary?</h5>
+            <h5><a href="#">Login</a></h5>
+            <h5><a href="#">Create Account</a></h5>
+            <img src={homeIcon} alt="Home Icon" />
         </>
     );
 };
